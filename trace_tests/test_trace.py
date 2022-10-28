e8a65fbc36a286a0e41d2d9c56694438a5a0a16c
Dong Yang <dong.yang@enlighten.finance>
2022-10-28 12:16:22 +0200

    feat(trace): test 4 traces

--- trace_tests/test_trace.py
+++ trace_tests/test_trace.py
@@ -1,60 +1,67 @@
 import json
-import sys
+import os
 
-if len(sys.argv) < 2:
-    print(f"Usage: {sys.argv[0]} <geth> [<blockno> <txn_hash>]")
-    exit(0)
+DIR = "/home/yangdong/chainnode-polygon/trace_tests/geth_traces"
+files = os.listdir(DIR)
+for fc, fname in enumerate(files):
+    print(f"Testing {fname}...")
+    output = []
+    with open(f"{DIR}/{fname}") as f:
+        rec = json.load(f)
+        def dfs(ct, path):
+            output.append({
+                "type": "call" if ct["type"] != "CREATE" else "create",
+                "action": {
+                    "callType": ct["type"].lower(),
+                    "from": ct["from"],
+                    "to": ct["to"],
+                    "gas": ct["gas"],
+                    "input": ct["input"],
+                    "value": "0x" if ("value" not in ct or int(ct["value"], 16) == 0) else ct["value"],
+                },
+                "result": {
+                    "gasUsed": ct["gasUsed"],
+                    "output": ct["output"],
+                },
+                "subtraces": len(ct["calls"]) if "calls" in ct else 0,
+                "traceAddress": path,
+                "blockHash": None,
+                "blockNumber": None,
+                "transactionHash": None,
+                "transactionPosition": None,
+                "transactionTraceID": 0,
+                "transactionLastTrace": 0
+            })
+            if "calls" in ct:
+                for i, c in enumerate(ct["calls"]):
+                    dfs(c, path + [i])
+        dfs(rec, [])
+        for i, d in enumerate(output):
+            d["transactionTraceID"] = i
+        output[-1]["transactionLastTrace"] = 1
 
-output = []
-with open(sys.argv[1]) as f:
-    rec = json.load(f)
-    def dfs(ct, path):
-        output.append({
-            "type": "call",
-            "action": {
-                "callType": ct["type"].lower(),
-                "from": ct["from"],
-                "to": ct["to"],
-                "gas": ct["gas"],
-                "input": ct["input"],
-                "value": "0x" if ("value" not in ct or int(ct["value"], 16) == 0) else ct["value"],
-            },
-            "result": {
-                "gasUsed": ct["gasUsed"],
-                "output": ct["output"],
-            },
-            "subtraces": len(ct["calls"]) if "calls" in ct else 0,
-            "traceAddress": path,
-            "blockHash": None,
-            "blockNumber": None,
-            "transactionHash": None,
-            "transactionPosition": None,
-            "transactionTraceID": 0,
-            "transactionLastTrace": 0
-        })
-        if "calls" in ct:
-            for i, c in enumerate(ct["calls"]):
-                dfs(c, path + [i])
-    dfs(rec, [])
-    for i, d in enumerate(output):
-        d["transactionTraceID"] = i
-    output[-1]["transactionLastTrace"] = 1
+    blockno, txhash = fname[:-5].split("_")
+    blockno = int(blockno)
 
-
-if len(sys.argv) == 4:
-    blockno = int(sys.argv[2])
-    txhash = sys.argv[3]
     with open(f"/home/yangdong/node/traces/{blockno//10000}/{blockno//100}.log") as f:
         traces = [json.loads(line) for line in f]
         txtraces = list(filter(lambda x: x["transactionHash"] == txhash, traces))
+
+        with open("expect.json", "w") as g:
+            for rec in output:
+                g.write(json.dumps(rec) + "\n")            
+
+        with open("output.json", "w") as g:
+            for rec in txtraces:
+                g.write(json.dumps(rec) + "\n")            
+
         assert len(txtraces) == len(output), f"Length differ: expect {len(output)}, get {len(txtraces)}"
         print(f"Number of traces: {len(output)}")
-        # for tr in txtraces:
-        #     print(tr)
         for dtrace, ctrace in zip(txtraces, output):
             ctrace["blockHash"] = dtrace["blockHash"]
             ctrace["blockNumber"] = dtrace["blockNumber"]
             ctrace["transactionHash"] = dtrace["transactionHash"]
             ctrace["transactionPosition"] = dtrace["transactionPosition"]
             assert ctrace == dtrace, f"Record differ\n{ctrace}\n{dtrace}"
-print("OK")
+    print(f"ok {fc+1}/{len(files)}")
+print("OK!")
