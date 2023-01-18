6f36efcb61d40533ad3eba4419e64321f5bb6699
Guodong@MeshEE <109643851+guodong-meshee@users.noreply.github.com>
2023-01-18 12:06:29 +0800

    Destill tag v0.2.17 (#4)
    * Dump ploygon data
    
    feat(trace): log sync step
    
    feat(trace): reuse data dump from our fork of geth
    
    feat(trace): verify dumped blocks & transactions
    
    feat(trace): remove precompile check (Wenhao)
    
    feat(trace): dump state sync txn/log + validate
    
    feat(trace): dump state sync trace + test
    
    feat(trace): test 4 traces
    
    Change buffer setting & simplify code
    * set perFolder, perFile to be 100000 1000
    * Merge state sync receipt and normal txn receipt
    * Use a global ParityLogger
    
    Merge receipt logging
    
    add github action
    
    * Measure time to write data to file.
    
    * Implement buffered write
    
    * Revert "Implement buffered write"
    
    Co-authored-by: mortal123 <lwhao2012@gmail.com>
    Co-authored-by: 0xDong <dong.yang@meshee.xyz>
    Co-authored-by: Dong <115082743+0xDong@users.noreply.github.com>

--- consensus/bor/bor.go
+++ consensus/bor/bor.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math"
 	"math/big"
 	"sort"
 	"strconv"
@@ -20,6 +21,7 @@ import (
 	"go.opentelemetry.io/otel/trace"
 	"golang.org/x/crypto/sha3"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/tracing"
@@ -33,6 +35,7 @@ import (
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/log"
@@ -1269,6 +1272,82 @@ func (c *Bor) getNextHeimdallSpanForTest(
 	}
 
 	return heimdallSpan, nil
+}
+
+//
+// Chain context
+//
+
+// chain context
+type chainContext struct {
+	Chain consensus.ChainHeaderReader
+	Bor   consensus.Engine
+}
+
+func (c chainContext) Engine() consensus.Engine {
+	return c.Bor
+}
+
+func (c chainContext) GetHeader(hash common.Hash, number uint64) *types.Header {
+	return c.Chain.GetHeader(hash, number)
+}
+
+// callmsg implements core.Message to allow passing it as a transaction simulator.
+type callmsg struct {
+	ethereum.CallMsg
+}
+
+func (m callmsg) From() common.Address { return m.CallMsg.From }
+func (m callmsg) Nonce() uint64        { return 0 }
+func (m callmsg) CheckNonce() bool     { return false }
+func (m callmsg) To() *common.Address  { return m.CallMsg.To }
+func (m callmsg) GasPrice() *big.Int   { return m.CallMsg.GasPrice }
+func (m callmsg) Gas() uint64          { return m.CallMsg.Gas }
+func (m callmsg) Value() *big.Int      { return m.CallMsg.Value }
+func (m callmsg) Data() []byte         { return m.CallMsg.Data }
+
+// get system message
+func getSystemMessage(toAddress common.Address, data []byte) callmsg {
+	return callmsg{
+		ethereum.CallMsg{
+			From:     systemAddress,
+			Gas:      math.MaxUint64 / 2,
+			GasPrice: big.NewInt(0),
+			Value:    big.NewInt(0),
+			To:       &toAddress,
+			Data:     data,
+		},
+	}
+}
+
+// apply message
+func applyMessage(
+	msg callmsg,
+	state *state.StateDB,
+	header *types.Header,
+	chainConfig *params.ChainConfig,
+	chainContext core.ChainContext,
+) error {
+	// Create a new context to be used in the EVM environment
+	blockContext := core.NewEVMBlockContext(header, chainContext, &header.Coinbase)
+
+	// Create a new environment which holds all relevant information
+	// about the transaction and calling mechanisms.
+	vmenv := vm.NewEVM(blockContext, vm.TxContext{}, state, chainConfig, vm.Config{Debug: true, Tracer: vm.GlobalParityLogger})
+	// Apply the transaction to the current state (included in the env)
+	_, _, err := vmenv.Call(
+		vm.AccountRef(msg.From()),
+		*msg.To(),
+		msg.Data(),
+		msg.Gas(),
+		msg.Value(),
+	)
+	// Update the state with pending changes
+	if err != nil {
+		state.Finalise(true)
+	}
+
+	return nil
 }
 
 func validatorContains(a []*valset.Validator, x *valset.Validator) (*valset.Validator, bool) {
